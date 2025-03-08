29d90a0c00a88cf304211cbc1ae4183e40ea4627
Trevor Manternach <trevormanternach@gmail.com>
2025-03-08 10:28:42 -0700

    Updated blogroll-convert.py to grab straight from FreshRSS instead of a local opml file

--- blogroll-convert.py
+++ blogroll-convert.py
@@ -1,7 +1,35 @@
+import requests
 import xml.etree.ElementTree as ET
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+'''
+url and authText need to be defined in your .env file
+url = "https://[FreshRSS URL]/api/greader.php/reader/api/0/subscription/export"
+authText = "GoogleLogin auth=username/hash"
+'''
+
+def send_request():
+    # Request (2)
+    # GET https://rss.home/api/greader.php/reader/api/0/subscription/export
+
+    try:
+        response = requests.get(
+            url=os.getenv('url'),
+            headers={
+                "Authorization": os.getenv('authText'),
+            }, verify=False
+        )
+        #print('Response HTTP Status Code: {status_code}'.format( status_code=response.status_code))
+        return response.content
+    except requests.exceptions.RequestException:
+        print('HTTP Request failed')
+
+opmlExport = send_request().decode("utf-8")
 
 # Load the OPML file
-tree = ET.parse("/Users/trevormanternach/Documents/Subscriptions-FreshRSS.opml")
+tree = ET.ElementTree(ET.fromstring(opmlExport))
 root = tree.getroot()
 
 categories = {}
@@ -18,14 +46,6 @@ for category in root.findall(".//outline[@text]"):
     if entries:
         categories[category_name] = entries
 
-
-
-# Format and write to Markdown
 for idx, (category, entries) in enumerate(categories.items(), start=1):
     print(f"## {category}\n")
     print("\n".join(entries) + "\n\n")
-
-
-#print("\n".join(markdown_list))
-
-#print("Markdown list successfully created as 'feeds_list.md'")
